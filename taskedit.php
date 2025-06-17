8c03f7cda6975ed24fbf74cbd8db5b9bd4e0203e
lut0035 <2301203@s.asojuku.ac.jp>
2025-06-17 10:56:45 +0900

    henkou

--- taskedit.php
+++ taskedit.php
@@ -1,33 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="taskedit.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="post">
 
-    <h1>タスク編集</h1><br>
+        <h1>タスク編集</h1>
+        <div class="taskall">
+           内容<br>
+            <textarea name="task_content" class="naiyo"></textarea><br>
 
-    内容:<input type="text" name="task_content"><br>
-    期限:<input type="date" name="task_deadline"><br>
+                期限<br>
+               
+                <input type="date" name="task_deadline" class=kigen><br>
+                
 
-    優先度:<select name="pr">
-<option value="pr1">優先度(低)</option>
-<option value="pr2">優先度(中)</option>
-<option value="pr3">優先度(高)</option>
-</select><br>
+                優先度<br>
+                <select name="pr" class="yusen">
+                    <option value="pr1">優先度(低)</option>
+                    <option value="pr2">優先度(中)</option>
+                    <option value="pr3">優先度(高)</option>
+                </select><br>
 
-状態:<select name="status">
-<option value="status1">未完了</option>
-<option value="status2">完了</option>
-</select><br>
+                状態<br>
+                <select name="status" class="jotai">
+                    <option value="status1">未完了</option>
+                    <option value="status2">完了</option>
+                </select><br>
 
- <button type="submit">保存</button><a href="">キャンセル</a>
- 
+                </div>
 
- </form>
+                <button type="submit">保存</button><a href="">キャンセル</a>
+       
+
+    </form>
 
 </body>
 
