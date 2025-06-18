9bf9b4a75786e32c3bf8f3245e18299e172984be
lut0035 <2301203@s.asojuku.ac.jp>
2025-06-18 12:36:37 +0900

    矢野は佐野

--- taskedit.php
+++ taskedit.php
@@ -9,35 +9,36 @@
 </head>
 
 <body>
-    <form action="" method="post">
+    <form action="" method="post" class="waku">
 
         <h1>タスク編集</h1>
         <div class="taskall">
-           内容<br>
+            内容<br>
             <textarea name="task_content" class="naiyo"></textarea><br>
 
-                期限<br>
-               
-                <input type="date" name="task_deadline" class=kigen><br>
-                
+            期限<br>
 
-                優先度<br>
-                <select name="pr" class="yusen">
-                    <option value="pr1">優先度(低)</option>
-                    <option value="pr2">優先度(中)</option>
-                    <option value="pr3">優先度(高)</option>
-                </select><br>
+            <input type="date" name="task_deadline" class=kigen><br>
 
-                状態<br>
-                <select name="status" class="jotai">
-                    <option value="status1">未完了</option>
-                    <option value="status2">完了</option>
-                </select><br>
 
-                </div>
+            優先度<br>
+            <select name="pr" class="yusen">
+                <option value="pr1">優先度(低)</option>
+                <option value="pr2">優先度(中)</option>
+                <option value="pr3">優先度(高)</option>
+            </select><br>
 
-                <button type="submit">保存</button><a href="">キャンセル</a>
-       
+            状態<br>
+            <select name="status" class="jotai">
+                <option value="status1">未完了</option>
+                <option value="status2">完了</option>
+            </select><br>
+
+        </div>
+
+        <div class="link">
+            <button type="submit">保存</button><a href="">キャンセル</a>
+        </div>
 
     </form>
 
