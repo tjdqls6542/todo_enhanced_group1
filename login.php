f8fa9af9dec929df692d9d82b3a8f2e9b2d2b3c3
kino-shita <2301155@s.asojuku.ac.jp>
2025-06-20 10:49:38 +0900

    login.php変更

--- login.php
+++ login.php
@@ -1,3 +1,22 @@
-<?php
-    //前のログイン画面そのまま使う
-?>
+<?php session_start(); ?>
+<?php //前のlogin.phpと一緒?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン</title>
+</head>
+<body>
+    <h2>ログイン</h2>
+
+    <?php if (isset($_GET['error']) && $_GET['error'] == '1'): ?>
+        <p>ユーザー名またはパスワードが間違っています。</p>
+    <?php endif; ?>
+
+    <form action="check.php" method="post">
+        <label>ユーザー名: <input type="text" name="username" value="たろう"></label><br><br>
+        <label>パスワード: <input type="password" name="password" value="aso111"></label><br><br>
+        <input type="submit" value="ログイン">
+    </form>
+</body>
+</html>
