d1d03e6adac2856108be4b74ea2da71078740049
kino-shita <2301155@s.asojuku.ac.jp>
2025-06-20 11:09:59 +0900

    logout.php変更

--- logout.php
+++ logout.php
@@ -1,1 +1,9 @@
- 
+ <?php
+session_start();
+$_SESSION = array();             // セッション変数を空にする
+session_destroy();               // セッションを破棄
+
+// ログインページにリダイレクト
+header("Location: login.php");
+exit;
+?>
