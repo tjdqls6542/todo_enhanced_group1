6df5021ce7fea2d27604d1cee2c60b6ce37bbc20
kino-shita <2301155@s.asojuku.ac.jp>
2025-06-23 10:52:30 +0900

    修正

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 session_start();
 
 $pdo = new PDO(
--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 session_start();
 $_SESSION = array();             // セッション変数を空にする
 session_destroy();               // セッションを破棄
