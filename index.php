1110b7dfcdc021663bf420b9359905fc70f31ecd
kino-shita <2301155@s.asojuku.ac.jp>
2025-07-04 12:14:56 +0900

    修正

--- index.php
+++ index.php
@@ -40,5 +40,44 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['username'])) {
     <section>
         <iframe src="search.php" title="タスク検索"></iframe>
     </section>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>タスク内容</th>
+            <th>状態</th>
+            <th>優先度</th>
+            <th>期限</th>
+            <th>操作</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>買い物に行く</td>
+            <td>未完了</td>
+            <td>高</td>
+            <td>2025-07-10</td>
+            <td>
+                <button>完了にする</button>
+                <button>削除</button>
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>レポート提出</td>
+            <td>完了</td>
+            <td>中</td>
+            <td>2025-07-05</td>
+            <td>
+                <button disabled>完了</button>
+                <button>削除</button>
+            </td>
+        </tr>
+        <!-- ここにPHPで繰り返し表示していく -->
+    </tbody>
+</table>
+
 </body>
 </html>
